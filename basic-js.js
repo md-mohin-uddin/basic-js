4e1b12a4668b2697e850e06b6336a6066d3569b4
Your Name <you@example.com>
2023-06-16 15:24:48 +0600

    again done

--- basic-js.js
+++ basic-js.js
@@ -142,3 +142,81 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+//Combine the if statements into a single if/else statement.
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+
+    if (val > 5) {
+        result = "Bigger than 5";
+    }
+
+    else {
+        result = "5 or Smaller";
+    }
+
+    // Only change code above this line
+    return result;
+}
+
+testElse(4);
+//Convert the logic to use else if statements.
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    }
+
+    else if (val < 5) {
+        return "Smaller than 5";
+    }
+    else {
+        return "Between 5 and 10";
+    }
+}
+
+testElseIf(7);
+
+//orderMyLogic
+function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+orderMyLogic(7);
+
+//Write chained if/else if statements to fulfill the following conditions:
+
+/**num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge **/
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return "Tiny";
+    }
+    else if (num < 10) {
+        return "Small";
+    }
+    else if (num < 15) {
+        return "Medium";
+    }
+    else if (num < 20) {
+        return "Large";
+    }
+    else {
+        return "Huge";
+    }
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+testSize(7);
