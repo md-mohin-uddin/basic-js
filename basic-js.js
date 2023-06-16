aa8e67d1aa5cf7ba050b61cedad8546a60ab01fb
Your Name <you@example.com>
2023-06-16 18:57:08 +0600

    some more exercise

--- basic-js.js
+++ basic-js.js
@@ -220,3 +220,79 @@ function testSize(num) {
 }
 
 testSize(7);
+
+
+
+/*Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.*/
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1) {
+        return names[0];
+    }
+    else if (strokes <= par - 2) {
+        return names[1];
+    }
+    else if (strokes === par - 1) {
+        return names[2];
+    }
+    else if (strokes === par) {
+        return names[3];
+    }
+    else if (strokes === par + 1) {
+        return names[4];
+    }
+    else if (strokes === par + 2) {
+        return names[5];
+    }
+    else if (strokes >= par + 3) {
+        return names[6];
+    }
+
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+golfScore(5, 4);
+/*
+Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta
+*/
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+
+    }
+
+
+    // Only change code above this line
+    return answer;
+}
+
+caseInSwitch(1);
+
